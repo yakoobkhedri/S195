bc2ea39912633fc88f59724cd479f01a720026f1
yakoobkhedri <yakkobkhedrii@gmail.com>
2026-08-28 01:57:57 +0330

    first commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+// open sidebar
+
+let openSidebar= document.getElementById('openSidebar');
+let sidebar= document.getElementById('sidebar');
+let closeSidebar= document.getElementById('closeSidebar');
+let overlay= document.getElementById('overlay');
+
+openSidebar.addEventListener('click', function () {
+    sidebar.classList.add('active');
+    overlay.classList.add('active');
+})
+closeSidebar.addEventListener('click', function () {
+    sidebar.classList.remove('active');
+        overlay.classList.remove('active');
+})
+overlay.addEventListener('click', function () {
+    sidebar.classList.remove('active');
+        overlay.classList.remove('active');
+})
+
+// accordion
 let accordionBtn= Array.from(document.getElementsByClassName('accordion-btn'));
 
 accordionBtn.forEach((item)=>{
